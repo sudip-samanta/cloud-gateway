59d13dd9fec998e537141e8a0cf26b36871d4a26
SUDIP SAMANTA <sudipsamanta@SUDIPs-MacBook-Air.local>
2021-10-21 20:04:08 +0530

    first commit

---
+++ src/test/java/com/fistic/cloud/gateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fistic.cloud.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
